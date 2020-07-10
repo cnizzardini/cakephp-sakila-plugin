23a9310e2bebce51feedb0699c1990752bcd2f26
chris cnizzardini <cnizzardini@gmail.com>
2020-07-09 23:20:44 -0400

    Added unit tests and travis file

---
+++ tests/TestCase/SakilaTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sakila\Test\TestCase;
+
+use Cake\TestSuite\ConsoleIntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+class SakilaTest extends TestCase
+{
+    use ConsoleIntegrationTestTrait;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->setAppNamespace('TestApp');
+        $this->useCommandRunner();
+
+        $this->controllers = APP . 'Controller' . DS;
+
+        foreach (scandir($this->controllers) as $file) {
+            if (!is_file($this->controllers . $file)) {
+                continue;
+            }
+            unlink($this->controllers . $file);
+        }
+    }
+
+    public function testMigrate()
+    {
+        $this->exec('migrations migrate -p Sakila');
+        $this->assertExitSuccess();
+
+        $this->exec('migrations seed -p Sakila');
+        $this->assertExitSuccess();
+    }
+}
